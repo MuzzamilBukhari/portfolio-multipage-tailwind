6cdc6fd4b30cf5c4ed999335e3a4c9ed713ed664
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-12-20 22:07:16 +0500

    fix styling

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,26 +5,31 @@ import EducationSection from "./EducationSection";
 
 const About = () => {
   return (
-    <section className="text-dark_bg2 dark:text-light_bg1 bg-light_bg1 dark:bg-dark_bg1 opacity-[0.99] body-font pt-12 sm:pt-24">
+    <section className="text-dark_bg2 dark:text-light_bg1 bg-light_bg1 dark:bg-dark_bg1 opacity-[0.99] body-font pt-20 sm:pt-24">
       <div className=" container mx-auto py-12 sm:py-24">
-        <div className="flex justify-center items-center gap-12 flex-col px-12 py-20 md:pt-0 md:flex-row">
-          <div className="w-[300px] rounded-xl" data-aos="flip-left">
+        <h1
+          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold title-font text-center"
+          data-aos="zoom-in"
+        >
+          About{" "}
+          <span className="text-light_primary dark:text-dark_primary underline">
+            me
+          </span>
+        </h1>
+        <div className="flex justify-center items-center gap-12 flex-col px-12  py-20 lg:flex-row">
+          <div className="w-[300px] h-full rounded-xl" data-aos="flip-left">
             {/* Image */}
-            <Image src={profile} alt={"profile"} className=" rounded-3xl" />
+            <Image
+              src={profile}
+              alt={"profile"}
+              className=" rounded-3xl"
+              height={700}
+            />
           </div>
           {/* Section Title */}
-          <div className="pt-12 flex justify-center items-center  md:items-start flex-col">
-            <h1
-              className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold title-font text-center"
-              data-aos="zoom-in"
-            >
-              About{" "}
-              <span className="text-light_primary dark:text-dark_primary underline">
-                me
-              </span>
-            </h1>
+          <div className=" flex justify-center items-center  md:items-start flex-col">
             <div
-              className="flex justify-center items-center flex-col gap-3 text-dark_bg2 dark:text-light_bg1 text-lg leading-relaxed text-left mx-auto max-w-[600px] mt-8"
+              className="flex justify-center items-center flex-col gap-3 text-dark_bg2 dark:text-light_bg1 text-base leading-relaxed text-left mx-auto max-w-[600px] mt-8"
               data-aos="fade-up"
               data-aos-duration="2000"
             >
